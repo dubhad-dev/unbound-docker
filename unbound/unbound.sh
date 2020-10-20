ad7f6dfcf4ef02e599cf794229ba058c564161c5
dubhad <dubhad.connect@gmail.com>
2020-10-20 14:08:59 +0300

    feg

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir -p /usr/local/etc/unbound/dev && \
-cp -a /dev/random /dev/urandom /dev/null /usr/local/etc/unbound/dev/
-
 /usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/root.key
 
-exec /usr/local/sbin/unbound 
+exec /usr/local/sbin/unbound -d -c /usr/local/etc/unbound/unbound.conf
