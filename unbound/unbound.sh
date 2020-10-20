172e637d45958c44c071383bf0455e1418fe8af8
dubhad <dubhad.connect@gmail.com>
2020-10-20 14:07:21 +0300

    fg

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -3,8 +3,6 @@
 mkdir -p /usr/local/etc/unbound/dev && \
 cp -a /dev/random /dev/urandom /dev/null /usr/local/etc/unbound/dev/
 
-mkdir -p -m 700 /usr/local/etc/unbound/var && \
-chown _unbound:_unbound /usr/local/etc/unbound/var && \
-/usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/var/root.key
+/usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/root.key
 
-exec /usr/local/sbin/unbound -d -c /usr/local/etc/unbound/unbound.conf
+exec /usr/local/sbin/unbound 
