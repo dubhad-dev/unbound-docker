da82c26366dc7184577db9bbe15169e121871229
dubhad <dubhad.connect@gmail.com>
2020-10-20 14:16:05 +0300

    Fix typo

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p -m 700 /usr/local/etc/unbound/var && \
-chown _unbound:_unbound /usr/local/etc/unbound/var && \
+chown unbound:unbound /usr/local/etc/unbound/var && \
 /usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/var/root.key
 
 exec /usr/local/sbin/unbound
