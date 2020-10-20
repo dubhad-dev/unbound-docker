6e78dc6c8f0afc73c211cefb1ea9f9506ccd1159
dubhad <dubhad.connect@gmail.com>
2020-10-20 14:04:33 +0300

    Script modified

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-/usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/root.key
+mkdir -p /usr/local/etc/unbound/dev && \
+cp -a /dev/random /dev/urandom /dev/null /usr/local/etc/unbound/dev/
 
-exec /usr/local/sbin/unbound -d 
+mkdir -p -m 700 /usr/local/etc/unbound/var && \
+chown _unbound:_unbound /usr/local/etc/unbound/var && \
+/usr/local/sbin/unbound-anchor -a /usr/local/etc/unbound/var/root.key
+
+exec /usr/local/sbin/unbound -d -c /usr/local/etc/unbound/unbound.conf
